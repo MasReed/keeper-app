57add433318838a61c8fc35fad3e166d862fb485
MasReed <bluekitdrummer@gmail.com>
2021-03-19 11:22:31 -0500

    Implement add note

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CreateArea from './CreateArea.jsx';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Note from './Note.jsx';
-import notes from '../notes.js';
+import TEMPnotes from '../notes.js';
 
 
 function App() {
+
+    const [allNotes, setAllNotes] = useState(TEMPnotes);
+
+    //update stateful array of notes
+    function getNewNote(title, content) {
+        setAllNotes( (prevNotes) => {
+            return ([
+                ...prevNotes,
+                {
+                    title: title,
+                    content: content
+                }
+            ])
+        });
+    }
+
     return (
         <div>
             <Header />
-            {notes.map(note => (
+            <CreateArea
+                getNewNote={getNewNote}
+            />
+            {allNotes.map(note => (
                 <Note
                     key={note.key}
                     title={note.title}
---
+++ keeper-app/src/components/CreateArea.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+function CreateArea(props) {
+
+    const [noteTitle, setNoteTitle] = useState('');
+    const [noteContent, setNoteContent] = useState('');
+
+    function handleTitleChange(event) {
+        setNoteTitle(event.target.value);
+    }
+
+    function handleContentChange(event) {
+        setNoteContent(event.target.value);
+    }
+
+    return (
+        <div>
+            <form>
+                <input name='title' onChange={handleTitleChange} placeholder='Title' value={noteTitle}/>
+                <textarea name='content' onChange={handleContentChange} placeholder='Take a note...' rows='3' value={noteContent}/>
+                <button type='submit'
+                    onClick={ (e) => {
+                        e.preventDefault();
+                        props.getNewNote(noteTitle, noteContent);
+                        setNoteTitle('');
+                        setNoteContent('');
+                    }}
+                >Add</button>
+            </form>
+        </div>
+    );
+}
+
+export default CreateArea;
